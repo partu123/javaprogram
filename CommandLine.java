d24ecde24cddb1214cef4c90508a2076961ded73
partu123 <114059935+partu123@users.noreply.github.com>
2022-09-29 13:36:23 +0530

    Add files via upload

---
+++ CommandLine.java
@@ -0,0 +1,11 @@
+public class CommandLine
+{
+public static void main(String x[])
+{
+int a,b,c;
+a=Integer.parseInt(x[0]);
+b=Integer.parseInt(x[1]);
+c=a+b;
+System.out.printf("addition is %d\n",c);
+}
+}
