5af1bfbd6c5818378b40482634b2406bd42af11d
partu123 <114059935+partu123@users.noreply.github.com>
2022-09-29 13:38:01 +0530

    Add files via upload

---
+++ TwoDaimonalArray.java
@@ -0,0 +1,25 @@
+import java.util.*;
+public class TwoDaimonalArray
+{
+public static void main(String x[])
+{
+Scanner xyz=new Scanner(System.in);
+int a[][]=new int[3][3];
+for(int i=0;i<a.length;i++)
+{
+for(int j=0;j<a.length;j++)
+{
+a[i][j]=xyz.nextInt();
+}
+}
+System.out.println("display array");
+for(int i=0;i<a.length;i++)
+{
+for(int j=0;j<a.length;j++)
+{
+System.out.printf("%d\t",a[i][j]);
+}
+System.out.printf("\n");
+}
+}
+}
