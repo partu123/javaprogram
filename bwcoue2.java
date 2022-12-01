d9c3cfc36a09616e152607f019028d9fd8b0d2e2
partu123 <114059935+partu123@users.noreply.github.com>
2022-12-01 20:54:00 +0530

    Add files via upload

---
+++ bwcoue2.java
@@ -0,0 +1,20 @@
+package competitiveexam;
+import java.util.*;
+public class bwcoue2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+      List a=new ArrayList();
+      a.add("red");
+      a.add("blue");
+      a.add("yellow");
+      a.add("orage");
+      a.add("green");
+      Iterator e=a.iterator();
+      while(e.hasNext())
+      {
+    	  System.out.println(e.next());
+      }
+	}
+
+}
