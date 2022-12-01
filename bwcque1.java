341ee2c29b40a3f7350ae5f7778c646532d104b7
partu123 <114059935+partu123@users.noreply.github.com>
2022-12-01 20:58:44 +0530

    Add files via upload

---
+++ bwcque1.java
@@ -0,0 +1,17 @@
+package competitiveexam;
+import java.util.*;
+public class bwcque1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       ArrayList  a1=new ArrayList();
+       String str="15";
+       int x=Integer.parseInt(str);
+       String str1="20";
+       int y=Integer.parseInt(str1);
+       System.out.println("output:"+x*y);
+       
+       
+	}
+
+}
